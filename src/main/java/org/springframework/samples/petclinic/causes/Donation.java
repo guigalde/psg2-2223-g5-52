0647ef441932c2d86cc309bfec31784c5064d296
josruiper4 <josruiper4@alum.us.es>
2023-03-12 22:54:58 +0100

    A3.2.a Refs #53 Created Donation entity

--- src/main/java/org/springframework/samples/petclinic/causes/Donation.java
+++ src/main/java/org/springframework/samples/petclinic/causes/Donation.java
@@ -1,7 +1,28 @@
 package org.springframework.samples.petclinic.causes;
 
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import ch.qos.logback.core.net.server.Client;
+import lombok.Getter;
+import lombok.Setter;
+
 //details of each donation (date of donation, amount, and client).
 
+@Entity
+@Getter
+@Setter
 public class Donation {
+
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private LocalDate dateOfDonation;
+
+    private Double budget;
+
+    private Client client;
+
 
 }
